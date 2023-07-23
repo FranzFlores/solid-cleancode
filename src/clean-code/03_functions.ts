eb68829e1743d25dc4e8f9de08c0b4bb0bcaa7f4
Franz Andrés Flores <franz.flores@clipp.app>
2023-07-22 21:57:14 -0500

    Ejercicio funciones resuelto

--- src/clean-code/03_functions.ts
+++ src/clean-code/03_functions.ts
@@ -1,35 +1,43 @@
+interface Movie {
+    title: string;
+    description: string; 
+    rating: number; 
+    cast: string[];
+}
+
+
 (() => {
 
-    // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
-        console.log({ movieId });
+    function getMovieById( id: string ) {
+        console.log({ movieId: id });
     }
 
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
+    function getMovieCastById( id: string ) {
         console.log({ id });
     }
 
-    // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
-        console.log({ ActorId });
+    function getActorBioById( id: string ) {
+        console.log({ id });
     }
     
-    // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
-        console.log({ title, description, rating, cast });
+    function createMovie(movie: Movie) {
+        console.log(movie);
     }
 
-    // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+
+    function checkFullName(fullName:string) {
+        console.log({fullName});
+        return true;
+    }
+
+    function createActor( fullName: string, birthdate: Date ): boolean {
         
         // tarea asincrona para verificar nombre
-        // ..
-        // ..
+        checkFullName(fullName);
+
         if ( fullName === 'fernando' ) return false;
 
         console.log('Crear actor');
         return true;        
-
     }
 })();
