c90b0d01d03aeb7129972ee1da5437cd57b6e56d
Franz Andrés Flores <franz.flores@clipp.app>
2023-07-29 21:23:01 -0500

    inicio de ejercicio de función compleja

--- src/clean-code/03_functions.ts
+++ src/clean-code/03_functions.ts
@@ -40,4 +40,24 @@ interface Movie {
         console.log('Crear actor');
         return true;        
     }
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+        let result: number;
+
+        if ( isDead ) {
+            result = 1500;
+        } else {
+            if ( isSeparated ) {
+                result = 2500;
+            } else {
+                if ( isRetired ) {
+                    result = 3000;
+                } else {
+                    result = 4000; 
+                }
+            }
+        }
+        
+        return result;
+    }
 })();
