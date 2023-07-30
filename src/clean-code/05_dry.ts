07c1ace0ac2acd322dd56488f2f2e2d1644276f6
Franz Andrés Flores <franz.flores@clipp.app>
2023-07-30 12:56:08 -0500

    Aplicando principio DRY al ejercicio

--- src/clean-code/05_dry.ts
+++ src/clean-code/05_dry.ts
@@ -5,14 +5,29 @@ class Product {
         public name: string = '',
         public price: number = 0,
         public size: Size = ''
-    ){ }
+    ) { }
+
+    isProductReady(): boolean {
+        // recorrer las propiedades de la clase 
+        for (const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if ((<string><unknown>this[key]).length <= 0) throw new Error(`${key} está vacío`);
+                    break;
+                case 'number':
+                    if ((<number><unknown>this[key]) <= 0) throw new Error(`${key} es cero`);
+                    break;
+                default:
+                    throw new Error(`${typeof this[key]} is not supported`);       
+            }
+        }
+
+        return true;
+    }
 
     toString() {
-        // No se aplica DRY
-        if (this.name.length <= 0) throw new Error("name is empty");
-        if (this.price <= 0) throw new Error("price is zero");
-        if (this.size.length <= 0) throw new Error("size is empty");
-        return  `${this.name} (${this.price}) , ${this.size}`;
+        if (!this.isProductReady()) return;
+        return `${this.name} (${this.price}) , ${this.size}`;
     }
 }
 
